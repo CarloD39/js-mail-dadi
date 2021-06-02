2bcc2ebd9af5b292f5e1f3cb01707a732c5b37c0
CarloD39 <carlodurand@hotmail.it>
2021-06-03 00:13:31 +0200

    refactoring dadi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // // ------PRIMO-ESERCIZIO-EMAIL-------
 
 var ContactEmail = ['carlodurand@hotmail.it', 'micheledurand@hotmail.it', 'maurodurand@hotmail.it', 'giacomodurand@hotmail.it'];
-var email = prompt('inserisci email')
+var email = prompt('inserisci email');
 var check = false;
 
 var output = document.getElementById('emails');
@@ -19,31 +19,52 @@ if (check){
     output.innerHTML += 'lemail inserita NON è in lista'
 }
 
-// // ---------------DADI---------------
+// // ---DADI-- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//              Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-var outputFirst = document.getElementById('primo-numero');
-var outputSecond = document.getElementById('secondo-numero');
+// var outputFirst = document.getElementById('primo-numero');
+// var outputSecond = document.getElementById('secondo-numero');
 
 //two random numbers located in two different divs
-var FirstDice = Math.floor(Math.random() * 7);
-outputFirst.innerHTML = FirstDice;
+// var FirstDice = Math.floor(Math.random() * 6)+1;
+// outputFirst.innerHTML = FirstDice;
 
-var SecondDice = Math.floor(Math.random() * 7);
-outputSecond.innerHTML = SecondDice;
+// var SecondDice = Math.floor(Math.random() * 6)+1;
+// outputSecond.innerHTML = SecondDice;
 
-var resultGame = document.getElementById('result-game');
+// var resultGame = document.getElementById('result-game');
 
-if(FirstDice === SecondDice){
-    resultGame.innerHTML = 'Its a draw';
+// if(FirstDice === SecondDice){
+//     resultGame.innerHTML = 'Its a draw';
 
-} else if(FirstDice > SecondDice){
-    resultGame.innerHTML = 'The First win';
+// } else if(FirstDice > SecondDice){
+//     resultGame.innerHTML = 'The First win';
 
-} else{
-    resultGame.innerHTML = 'The Second win';
-}
+// } else{
+//     resultGame.innerHTML = 'The Second win';
+// }
 
-
+// ------------------------------------------------
    
 
+// // ---DADI-- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//              Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+var Result = document.getElementById('it');
+var FirstItem =  Math.floor(Math.random() * 6)+1;
+var SecondItem =  Math.floor(Math.random() * 6)+1;
+
+Result.innerHTML += FirstItem + '-------' + SecondItem;
+
+var FinalResult = document.getElementById('winner');
+
+if(FirstItem > SecondItem){
+    FinalResult.innerHTML = 'FirstItem is the winner';
+
+}else if(FirstItem < SecondItem){
+    FinalResult.innerHTML = 'SecondItem is the winner';
+
+}else{
+    FinalResult.innerHTML = 'its a draw';
+}
